1c24e390c0f378a7d80930a448dbc5a834b5520e
bumstheword <bumstheword@users.noreply.github.com>
2015-07-30 15:12:00 -0400

    Update travel.hs

--- travel.hs
+++ travel.hs
@@ -19,3 +19,19 @@ sum' (x:xs)
 main = do
     print $ [(h 10),(h 100),(h 1000), (h 10000), (h 100000),(h 1000000)]
     print $ product [(h 10),(h 100),(h 1000), (h 10000), (h 100000), (h 1000000)]
+
+
+isAbundant :: Int -> Bool
+isAbundant n = (sum . factors) n > n
+
+factors :: Int -> [Int]
+factors n = filter (\x -> mod n x == 0) [1..(div n 2)]
+
+isMult :: Int -> Bool
+isMult n = (filter isAbundant . factors) n
+
+main = do
+    (putStrLn . show . filter isAbundant) [1..100]
+    (putStrLn . show . filter isAbundant) [101..200]
+    (putStrLn . show . filter isAbundant) [201..300]
+    (putStrLn . show . sum . filter odd) [1..28123]
